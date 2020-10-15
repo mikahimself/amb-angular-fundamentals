e985d0fcc4ed6e6f62a7baa39d19e1fb88717774
mikahimself <mika.laihanen@gmail.com>
2020-10-15 09:16:05 +0300

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      {{ title }}
+    </div>
+  `
+}
+  
+)
+export class AppComponent {
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular'
+  }
+}
