28dcdf68c25ad92f06b566ce119dc67d985d67c9
mikahimself <mika.laihanen@gmail.com>
2020-10-23 08:31:34 +0300

    Compo architecture lesson 6 complete

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-count',
+  template: `
+    <div>
+      Count compo
+    </div>
+  `
+})
+
+export class PassengerCountComponent {
+  constructor() { }
+
+}
