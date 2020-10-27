d639f917b114e791d3810ebeebbe924b1ff83b54
mikahimself <mika.laihanen@gmail.com>
2020-10-27 21:46:55 +0200

    Template driven forms, inputs and validation lesson 8 complete.

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -7,8 +7,11 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['./passenger-viewer.component.scss'],
   template: `
     <div>
+      <!-- Here we are passing the passenger object to the detail @Input -->
+      <!-- and catching the update @output from passenger-form -->
       <passenger-form 
-        [detail]="passenger">
+        [detail]="passenger"
+        (update)="onUpdatePassenger($event)">
       </passenger-form>
     </div>
   `
@@ -23,5 +26,16 @@ export class PassengerViewerComponent implements OnInit {
       .subscribe((data: Passenger) => {
         this.passenger = data
       });
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        // Here we use Object.assign to create a new object by
+        // merging the changes against the current this.passenger
+        // and assigning the new passenger object to this.passenger.
+        this.passenger = Object.assign({}, this.passenger, event)
+      })
   }
 }
