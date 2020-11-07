04fe74107e0253a265956da2d3c0ea65f7e66480
mikahimself <mika.laihanen@gmail.com>
2020-11-07 14:16:37 +0200

    Component routing lesson 7 complete.

--- app/app.component.ts
+++ app/app.component.ts
@@ -44,6 +44,11 @@ export class AppComponent {
       exact: true
     },
     {
+      link: '/passengers',
+      name: 'Passengers',
+      exact: true
+    },
+    {
       link: '/oops',
       name: '404',
       exact: false
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 // Containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
@@ -14,6 +15,13 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 // Service 
 import { PassengerDashboardService } from './passenger-dashboard.service';
 
+const routes: Routes = [
+  {
+    path: 'passengers',
+    component: PassengerDashboardComponent
+  }
+]
+
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
@@ -25,12 +33,10 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
   imports: [
     CommonModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forChild(routes)
   ],
-  exports: [
-    PassengerDashboardComponent,
-    PassengerViewerComponent
-  ],
+  // Exports were removed because we use routing to get to the components.
   providers: [PassengerDashboardService]
 })
 export class PassengerDashboardModule {
